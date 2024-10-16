03718f344786df9538447f4b66144d5566e4bfa7
Fantasm4no <erickzhigue@gmail.com>
2024-10-16 11:23:06 -0500

    Primer commit

---
+++ src/Persona.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Persona {
+    
+    public void arregloPersonas(int[] arreglo){
+        String nombre;
+        int edad;
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de personas en el listado: ");
+        int tamanio = leer.nextInt();
+
+
+    }
+    
+
+    
+
+
+
+}
